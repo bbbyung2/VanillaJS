41e56729f2e46bab5e006c15dca3bfafc9767eef
bbbyung2 <bhko0524@naver.com>
2020-05-07 00:43:58 +0900

    #11 geolocation

---
+++ weather.js
@@ -0,0 +1,39 @@
+const API_KEY = "35968cf0aa0dce5c74e689412d42e8b3";
+const COORDS = "coodrs";
+
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude: latitude,
+        longitude: longitude
+    };
+    saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+    console.log("Can't access geo location.");
+}
+
+function askForCoords() {
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+    const loadedCoords = localStorage.getItem(COORDS);
+    if (loadedCoords === null) {
+        askForCoords();
+    } else {
+
+    }
+}
+
+function init() {
+    loadCoords();
+}
+
+init();
